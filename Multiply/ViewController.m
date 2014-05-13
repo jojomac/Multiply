dceaa29d952885f226c0dc2bbe1edc3c93e1b203
jojomac <joanne.mcnamee@d214.org>
2014-05-13 13:39:56 -0500

    This is hopefully the right ones.

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -9,16 +9,68 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *myNumber;
 
+@property (weak, nonatomic) IBOutlet UILabel *myMultiplier;
+@property (weak, nonatomic) IBOutlet UILabel *myAnswer;
+
+@property (weak, nonatomic) IBOutlet UISlider *myslider;
 @end
 
 @implementation ViewController
+
+
+- (IBAction)sliderMultiply:(id)sender {
+         NSLog(@"%f", sender.value);
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
+//slider code
+//-(IBAction)sliderchange:(UISlider *)sender
+//{
+//self.multiplier.text = [NSString
+//NSLog(@"%f", sender.value);
+//}
+- (IBAction)onCalculateButtonPressed:(id)sender
+{
+    int number = [self.myNumber.text intValue];
+    NSInteger multiple = [self.myMultiplier.text integerValue];
+    NSInteger result = number * multiple;
+
+
+    if(result > 20)
+    {
+        self.view.backgroundColor = [UIColor greenColor];
+    }
+    else{
+        self.view.backgroundColor = [UIColor whiteColor];
+
+    }
+
+    if ((result %3 == 0) && (result %5 == 0))
+    {
+        self.myAnswer.text = @"fizzbuzz";
+    }
+    else if (result %3 == 0)
+    {
+        self.myAnswer.text = @"fizz";
+    }
+    else if (result %5 == 0)
+    {
+        self.myAnswer.text = @"buzz";
+    }
+    else
+        self.myAnswer.text = [NSString stringWithFormat:@"%d", result];
+
+    //gets rid of keyboard;
+    [self.myNumber resignFirstResponder];
+}
+
 
 - (void)didReceiveMemoryWarning
 {
